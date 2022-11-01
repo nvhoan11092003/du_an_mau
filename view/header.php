67d992c2ac02ecff9c898f1d1c333a2f363afac0
nvhoan11092003 <nvhoan11092003@gmail.com>
2022-11-01 08:56:39 +0700

    test

--- view/header.php
+++ view/header.php
@@ -8,7 +8,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="view/slide.css">
 </head>
-<body>
+<body>  
     <!-- content -->
     <div class="mx-auto w-11/12">
        <a href="index.php"><h1 class="text-5xl m-3">
@@ -17,7 +17,7 @@
         <!-- menu -->
         <nav class="mb-2">
             <ul class="flex bg-black text-white font-bold">
-                <li><a href="index.php" class="p-4 block hover:text-red-600 hover:underline">Trang Chủ</a></li>
+                <li><a href="index.php" class="p-4 block hover:text-blue-500 hover:underline">Trang Chủ</a></li>
                 <li><a href="index.php?act=gt" class="p-4 block hover:text-red-600 hover:underline">Giới Thiệu</a></li>
                 <li><a href="index.php?act=lh" class="p-4 block hover:text-red-600 hover:underline">Liên Hệ</a></li>
                 <li><a href="index.php?act=gy" class="p-4 block hover:text-red-600 hover:underline">Góp ý</a></li>
